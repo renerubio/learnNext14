36e4c72726cedde6de41c7bb0c199e719ed05c24
René Rubio Muñoz <rrubio@intekmedical.com>
2023-11-07 12:05:03 +0100

    chore: part of chapter 12 https://nextjs.org/learn/dashboard-app/mutating-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        Test to update automatic deployment{children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   )
 }
